c9a24bb1d8eaa0a963d944f9577e3cdb9e02f42c
Nikita Titov <nmtitov@nmtitov.com>
2016-02-11 20:42:36 +0200

    Map to Int earlier

--- DoubleClickSwift/ViewController.swift
+++ DoubleClickSwift/ViewController.swift
@@ -19,10 +19,10 @@ class ViewController: UIViewController {
         
         
         let touch = button.rac_signalForControlEvents(.TouchUpInside)
-        let touches = touch.takeUntil(touch.throttle(0.25)).collect().`repeat`()
+        let touches = touch.takeUntil(touch.throttle(0.25)).collect().map({ $0.count }).`repeat`()
         
-        let click = touches.filter { $0.count == 1 }.mapReplace("Click")
-        let clicks = touches.filter { $0.count >= 2 }.map { "Clicks: \($0.count)" }
+        let click = touches.filter { $0 as! Int == 1 }.mapReplace("Click")
+        let clicks = touches.filter { $0 as! Int >= 2 }.map { "Clicks: \($0)" }
         let clear = RACSignal.merge([click, clicks]).throttle(1).mapReplace("")
         
         let all = RACSignal.merge([click, clicks, clear])
