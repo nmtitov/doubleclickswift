4c433db153ffe5a1885da1631aa1e063b9c7114f
Nikita Titov <nmtitov@nmtitov.com>
2016-02-12 00:51:59 +0200

    replaced `throttle` with `flatMap`, it works!

--- DoubleClickSwift/ViewController.swift
+++ DoubleClickSwift/ViewController.swift
@@ -20,12 +20,20 @@ class ViewController: UIViewController {
         
         
         let touch = button.rac_signalForControlEvents(.TouchUpInside).toSignalProducer().flatMapError { _ in SignalProducer<AnyObject?, NoError>.empty }
-        let throttle = touch.throttle(0.25, onScheduler: QueueScheduler.mainQueueScheduler).map { _ in () }
-        let touches = touch.takeUntil(throttle).collect().times(Int.max).map { $0.count }
+
+        let delay = touch.flatMap(.Latest, transform: { next in
+            SignalProducer(value: next).delay(0.25, onScheduler: QueueScheduler.mainQueueScheduler)
+        }).map { _ in () }
+        
+        let touches = touch.takeUntil(delay).collect().times(Int.max).map { $0.count }
         
         let click = touches.filter { $0 == 1 }.map { _ in "Click" }
         let clicks = touches.filter { $0 >= 2 }.map { "Clicks: \($0)" }
-        let clear = touches.throttle(1, onScheduler: QueueScheduler.mainQueueScheduler).map { _ in "" }
+        
+        let clear = touches.flatMap(.Latest, transform: { next in
+            SignalProducer(value: next).delay(1, onScheduler: QueueScheduler.mainQueueScheduler)
+        }).map { _ in "" }
+        
         let text = SignalProducer(values: [click, clicks, clear]).flatten(.Merge)
         
         DynamicProperty(object: label, keyPath: "text") <~ text.map { $0 }
